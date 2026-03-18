b4e9c00e68972492af56d407a9b7ac22488a7ed6
rafaelbaluartebaldoceda-eng <rafael.baluarte.baldoceda@gmail.com>
2026-03-18 02:45:26 -0500

    Actualizo modelo Libro completo con setter, str, repr y docstrings

--- models/libro.py
+++ models/libro.py
@@ -1,18 +1,37 @@
 class Libro:
+    """Representa los atributos del libro en el sistema de inventario de libreria"""
     def __init__(self,id,titulo,autor,disponible=True):
+        """Inicializa un libro con los atributos basicos."""
         self._id=id
         self._titulo=titulo
         self._autor=autor
         self._disponible=disponible
     @property
     def id(self):
+        """Devuelve el valor asignado al id del libro"""
         return self._id
     @property
     def titulo(self):
+        """Retorna el titulo del libro"""
         return self._titulo
     @property
     def autor(self):
+        """Retorna el nombre del autor del libro"""
         return self._autor
     @property
     def disponible(self):
+        """Retorna el estado de disponibilidad del libro"""
         return self._disponible
+    @disponible.setter
+    def disponible(self,disponible):
+        """Se añade metodo que actualiza el estado de disponibilidad del libro"""
+        if not isinstance(disponible,bool):
+            raise TypeError("Valor incorrecto, no esta definido como booleano")
+        self._disponible=disponible
+    def __str__(self):
+        """Retorna la informacion completa del libro para el usuario"""
+        estado= "Disponible" if self._disponible else "No Disponible"
+        return f"Libro {self._titulo} - {self._autor} ({estado})"
+    def __repr__(self):
+        """Retorna la informacion tecnica de los atributos del libro"""
+        return f"Libro(id={self._id}, titulo={self._titulo}, autor={self._autor}, disponible={self._disponible})"
